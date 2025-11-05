556420e871ee62b77398c18221be6c0ffcd6b4d2
valentina23-ai <vogueosorio23@gmail.com>
2025-11-05 17:24:27 -0500

    03_base_datos_comic

---
+++ 03_base_datos_comic/bd.js
@@ -0,0 +1,33 @@
+let comic ={
+
+    
+  "NombreComic": "Huellas de ritmo",
+  "numeroCapitulos": 3,
+  "genero": "Fantasia y drama",
+  "sipnosis": "narra la historia de Isabel, una niña prodigiosa en el baile que vive en una isla junto a su pueblo. Este lugar enfrenta el desafío de olvidar su herencia musical, lo cual afecta profundamente al guardián del pueblo.",
+  "active": true,
+ "autores": [ "valentina", "Jimena", "carolina"]
+  "capitulos": [
+    {
+      "id": 1,
+      "nombre": ,
+      "personajes": "Dan Jukes",
+      "portada": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+     "descripcion":
+    },
+    {
+      
+     "id": 1,
+      "nombre": 29,
+      "personajes": "Dan Jukes",
+      "portada": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+     "descripcion"
+    },
+    {
+       "id": 1,
+      "nombre": 29,
+      "personajes": "Dan Jukes",
+      "portada": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+     "descripcion"}
+      ]
+    }
