197b3884d9f3e429929dece0e95451e57df0051a
valentina23-ai <vogueosorio23@gmail.com>
2025-11-24 17:17:29 -0500

    index y css nuevos

--- 03_base_datos_comic/bd.js
+++ 03_base_datos_comic/bd.js
@@ -6,29 +6,32 @@ let comic = {
   "active" : true,
   "autores" : ["Valentina", "Jimena", "Carolina"],
 
-  "capitulos": [
+"capitulos": [
     {
-      "id": 1,
-      "nombre": "Capítulo 1",
-      "personajes": ["Dan Jukes"],
-      "portada": ["Radiation resistance", "Turning tiny", "Radiation blast"],
-      "descripcion": "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. "
+      "id": "capitulo numero 1",
+      "nombre": "Los ritmos de las maracas y un sueño.",
+      "personaje": "shakira,",
+      "descripcion": ["en una isla nace una pequeña niña llamada isabel, la cual es unab gran bailarina y",
+      "recibe unas maracas que resuenan con su grandiosa forma de bailar por lo cual es apodada SHAKIRA. todo va bien en esta isla musical la cual siempre esta rodeada de musica y canto"]
     },
+    
     {
-      "id": 2,
-      "nombre": "Capítulo 2",
-      "personajes": ["Dan Jukes"],
-      "portada": ["Radiation resistance", "Turning tiny", "Radiation blast"],
-      "descripcion": "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. "
+      "id": "capitulo numero 2",
+      "nombre": "el canto.",
+      "personaje": "shakira, lobo",
+      "descripcion": ["pasan los años y la isla empieza a perder su interes por la musiica  el baile, la isla se va quedando en silencio poco a poco mientras nuestra protagonista tiene problemas para encajar con los de su edad por seguir sintiendo el ritmo de la musica por sus venas.",
+      "shakira tiene un sueño con lobo el guardián del pueblo el cual le dice que la musica y el baile son su herencia y que debe protegerla de la oscuridad que desea apoderaerse de la isla."]
+      },
+    
+    {
+      "id": "capitulo numero 3",
+      "nombre": "las sombras y el ritual final.",
+      "personaje": "shakira, lobo",
+      "descripcion": ["shakira obtiene un collar magico que la une a la isla y a el poco poder que tiene gracias a la musica y junto con esto decide enfrentar su miedo a sociaizar y reune a el pueblo para hacer una fiesta y asi recordar sus construmbres pero todo se ve interrumpido por la llegada de la oscuridad que toma al pueblo, pero en ese instante aparece un lobo sobrenatural que la ayuda a derrotar a la oscuridad a pesar de llegar debilitado a la batalla."]
     },
-    {
-      "id": 3,
-      "nombre": "Capítulo 3",
-      "personajes": ["Dan Jukes"],
-      "portada": ["Radiation resistance", "Turning tiny", "Radiation blast"],
-      "descripcion": "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. "
-    }
-  ],
+     
+  ]
+
 
   "personajes": [
     {
