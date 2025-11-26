205be7500b3671be73391d15a10b0909905e11e7
valentina23-ai <vogueosorio23@gmail.com>
2025-11-26 17:20:27 -0500

    medio avancé

--- 03_base_datos_comic/app.js
+++ 03_base_datos_comic/app.js
@@ -7,10 +7,13 @@ comic.personajes.forEach(char => {
 const section = document.createElement('section');
 section.classList.add('galery')
 section.innerHTML `
+<a href="index2.html?id=${char.id}">
 <img src="${char.Imagen}"
 <h3>${char.Nombre}</h3>
 <p>${char.Descripcion}</p>
+</a>
 `;
 Numeropersonajes.appendChild(div);
 
 });
+
